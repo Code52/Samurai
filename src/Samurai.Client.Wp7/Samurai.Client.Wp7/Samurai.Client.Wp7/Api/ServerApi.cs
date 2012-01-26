6ca9cd7a8af147138625f8b887222f21f14144a7
Chr0n1x <michael@mquandt.com>
2012-01-27 00:09:16 +1100

    Updated API code in XNA project to match capabilities of the console client

--- src/Samurai.Client.Wp7/Samurai.Client.Wp7/Samurai.Client.Wp7/Api/ServerApi.cs
+++ src/Samurai.Client.Wp7/Samurai.Client.Wp7/Samurai.Client.Wp7/Api/ServerApi.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
-using Newtonsoft.Json;
 using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
 using SamuraiServer.Data;
 
 namespace Samurai.Client.Wp7.Api
@@ -13,42 +11,133 @@ namespace Samurai.Client.Wp7.Api
     {
         private string serverUrl;
 
-        public ServerApi(string serverUrl) {
+        public ServerApi(string serverUrl)
+        {
             this.serverUrl = serverUrl;
         }
 
-        public void GetListOfGames(Action<IEnumerable<GameState>, Exception> callback) {
-            Get("/api/ListGames", callback);
+        public void CreatePlayer(string name, Action<CreatePlayerResponse, Exception> callback)
+        {
+            Post("/Api/Players/CreatePlayer", "name=" + System.Uri.EscapeDataString(name), callback);
         }
 
-        private void Get<T>(string url, Action<T, Exception> callback) {
-            try {
+        public void GetOpenGames(Action<GetOpenGamesResponse, Exception> callback)
+        {
+            Get("/Api/Games/GetOpenGames", callback);
+        }
+
+        public void CreateGameAndJoin(string name, Guid playerId, Action<CreateGameAndJoinResponse, Exception> callback)
+        {
+            Post("/Api/Games/CreateGameAndJoin", "name=" + System.Uri.EscapeDataString(name) + "&playerid=" + playerId, callback);
+        }
+
+        public void JoinGame(Guid gameId, Guid playerId, Action<CreateGameAndJoinResponse, Exception> callback)
+        {
+            Post("/Api/Games/JoinGame", "gameid=" + gameId + "&playerid=" + playerId, callback);
+        }
+
+        private void Get<T>(string url, Action<T, Exception> callback)
+        {
+            try
+            {
                 var request = HttpWebRequest.CreateHttp(Uri(url));
-                request.BeginGetResponse(_ => {
-                    try {
+                request.BeginGetResponse(_ =>
+                {
+                    try
+                    {
                         var response = request.EndGetResponse(_) as HttpWebResponse;
                         if (response.StatusCode != HttpStatusCode.OK) callback(default(T), new Exception("Unexpected server response " + response.StatusCode));
 
                         string json;
-                        using (var reader = new StreamReader(response.GetResponseStream())) {
-                            json= reader.ReadToEnd();
+                        using (var reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            json = reader.ReadToEnd();
                             reader.BaseStream.Close();
                         }
                         response.Close();
 
                         T data = JsonConvert.DeserializeObject<T>(json);
                         callback(data, null);
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e)
+                    {
                         callback(default(T), e);
                     }
                 }, null);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 callback(default(T), e);
             }
         }
 
-        private Uri Uri(string relativePath) {
+        private void Post<T>(string url, string data, Action<T, Exception> callback)
+        {
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(data);
+                var request = (HttpWebRequest)HttpWebRequest.Create(Uri(url));
+                request.Method = "POST";
+                request.Accept = "application/json";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.BeginGetResponse(_ =>
+                {
+                    try
+                    {
+                        var response = request.EndGetResponse(_) as HttpWebResponse;
+                        if (response.StatusCode != HttpStatusCode.OK) callback(default(T), new Exception("Unexpected server response " + response.StatusCode));
+
+                        string json;
+                        using (var reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            json = reader.ReadToEnd();
+                            reader.BaseStream.Close();
+                        }
+                        response.Close();
+
+                        T convertedData = JsonConvert.DeserializeObject<T>(json);
+                        callback(convertedData, null);
+                    }
+                    catch (Exception e)
+                    {
+                        callback(default(T), e);
+                    }
+                }, null);
+            }
+            catch (Exception e)
+            {
+                callback(default(T), e);
+            }
+        }
+
+        private Uri Uri(string relativePath)
+        {
             return new Uri(new Uri(serverUrl), relativePath);
         }
+    }
+
+    public class ServerResponse
+    {
+        public bool Ok { get; set; }
+    }
+
+    public class GetOpenGamesResponse : ServerResponse
+    {
+        public GameState[] Games { get; set; }
+    }
+
+    public class CreateGameAndJoinResponse : ServerResponse
+    {
+        public GameState Game { get; set; }
+    }
+
+    public class JoinGameResponse : ServerResponse
+    {
+        public GameState Game { get; set; }
+    }
+
+    public class CreatePlayerResponse : ServerResponse
+    {
+        public Player Player { get; set; }
     }
 }
