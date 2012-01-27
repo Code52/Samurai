418b44a9c1b42dcde6d2e7db39f8acb1767bb2c1
Brendan Forster <me@brendanforster.com>
2012-01-27 22:51:36 +1100

    code cleanup

--- src/SamuraiServer/SamuraiServer.Data/Providers/PlayersProvider.cs
+++ src/SamuraiServer/SamuraiServer.Data/Providers/PlayersProvider.cs
@@ -1,53 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace SamuraiServer.Data
 {
     public class PlayersProvider : IPlayersProvider
     {
-        private readonly IPlayerRepository _repo;
+        private readonly IPlayerRepository repo;
 
         public PlayersProvider(IPlayerRepository repository)
         {
-            _repo = repository;
+            repo = repository;
         }
 
         public IEnumerable<Player> GetLeaderboard(int page, int players)
         {
-            return _repo.GetAll().Where(p => p.IsActive).OrderByDescending(p => p.Wins).Skip(page*players).Take(players);
+            return repo.GetAll().Where(p => p.IsActive).OrderByDescending(p => p.Wins).Skip(page * players).Take(players);
         }
 
         public ValidationResult<Player> Create(string name)
         {
-            if (_repo.GetByName(name) != null) return ValidationResult<Player>.Failure("User already exists");
+            if (repo.GetByName(name) != null) return ValidationResult<Player>.Failure("User already exists");
 
             var random = new Random();
 
-            var player = new Player{Name = name};
-            
-            player.Id = Guid.NewGuid();
-            player.IsOnline = true;
-            player.LastSeen = DateTime.Now;
-            player.ApiKey = string.Format("{0}{1}{2}{3}{4}",
-                                     random.Next(0, 9),
-                                     random.Next(0, 9),
-                                     random.Next(0, 9),
-                                     random.Next(0, 9),
-                                     random.Next(0, 9));
+            var player = new Player
+                             {
+                                 Name = name,
+                                 Id = Guid.NewGuid(),
+                                 IsOnline = true,
+                                 LastSeen = DateTime.Now,
+                                 ApiKey = string.Format("{0}{1}{2}{3}{4}",
+                                                        random.Next(0, 9),
+                                                        random.Next(0, 9),
+                                                        random.Next(0, 9),
+                                                        random.Next(0, 9),
+                                                        random.Next(0, 9)),
+                                 IsActive = true
+                             };
 
-            player.IsActive = true;
-
-            _repo.Add(player);
-            _repo.Save();
+            repo.Add(player);
+            repo.Save();
 
             return ValidationResult<Player>.Success.WithData(player);
         }
 
         public ValidationResult<Player> Login(string name, string apiKey)
         {
-            Player player = _repo.GetByName(name);
+            Player player = repo.GetByName(name);
 
             // See if player 1) wasn't found or 2) ApiKey doesn't match
             if (player == null || string.CompareOrdinal(apiKey, player.ApiKey) != 0)
@@ -60,7 +60,7 @@ namespace SamuraiServer.Data
 
         public Player Get(Guid id)
         {
-            return _repo.Get(id);
+            return repo.Get(id);
         }
 
         public void Invite(Guid id)
@@ -70,14 +70,14 @@ namespace SamuraiServer.Data
 
         public void Ban(Guid id)
         {
-            var player = _repo.GetAll().FirstOrDefault(p => p.IsActive && p.Id == id);
-            if(player != null) player.IsActive = false;
-            _repo.Save();
+            var player = repo.GetAll().FirstOrDefault(p => p.IsActive && p.Id == id);
+            if (player != null) player.IsActive = false;
+            repo.Save();
         }
 
         public IEnumerable<Player> Search(string searchTerm)
         {
-            var players = _repo.FindBy(p => p.IsActive && p.Name.Contains(searchTerm));
+            var players = repo.FindBy(p => p.IsActive && p.Name.Contains(searchTerm));
 
             return players;
         }
--- src/SamuraiServer/SamuraiServer/Areas/Api/Controllers/PlayersController.cs
+++ src/SamuraiServer/SamuraiServer/Areas/Api/Controllers/PlayersController.cs
@@ -6,51 +6,44 @@ namespace SamuraiServer.Areas.Api.Controllers
 {
     public class PlayersController : Controller
     {
-        //
-        // GET: /Api/Players/
-
-        private readonly IPlayersProvider _prov;
+        private readonly IPlayersProvider prov;
 
         public PlayersController(IPlayersProvider prov)
         {
-            _prov = prov;
+            this.prov = prov;
         }
 
-        [Api]
         public ActionResult Leaderboard()
         {
-            var leaders = _prov.GetLeaderboard(0, 20);
+            var leaders = prov.GetLeaderboard(0, 20);
 
-            return View(leaders);
+            return Json(new { ok = true, leaders}, JsonRequestBehavior.AllowGet);
         }
 
-        [Api]
         [HttpPost]
         public ActionResult CreatePlayer(string name)
         {
-            var result = _prov.Create(name);
-            if (result.IsValid == false) return View(new { ok = false, message = result.Message });
+            var result = prov.Create(name);
+            if (result.IsValid == false) 
+                return Json(new { ok = false, message = result.Message });
 
-            return View(new { ok = true, player = result.Data });
+            return Json(new { ok = true, player = result.Data });
         }
 
-        [Api]
         [HttpPost]
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
         public ActionResult Login(string name, string token)
         {
-            var result = _prov.Login(name, token);
+            var result = prov.Login(name, token);
 
             if (result.IsValid == false)
             {
                 // Login didn't go through
-                return View(new { ok = false, message = result.Message });
+                return Json(new { ok = false, message = result.Message });
             }
-            else
-            {
-                // Login ok, send the player-object back
-                return View(new { ok = true, player = result.Data });
-            }
+            
+            // Login ok, send the player-object back
+            return Json(new { ok = true, player = result.Data });
         }
     }
 }
