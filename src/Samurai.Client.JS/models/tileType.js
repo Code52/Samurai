3bba1f921f127472f5a2bfc2e7ed02873367c462
Arthur <partylich@gmail.com>
2012-11-17 18:39:17 -0500

    Added concrete tile objects (water, grass, etc)

--- src/Samurai.Client.JS/models/tileType.js
+++ src/Samurai.Client.JS/models/tileType.js
@@ -8,5 +8,51 @@ function TileType() {
 }
 
 TileType.prototype.toString = function () {
-  //TODO: Implement a string representation of this obj.
+  //TODO: String representation of this base obj?
+};
+
+//public class Grass : TileType
+function Grass() {
+    this.name = 'Grass';
+    this.canMoveOn = true;
+    this.canShootOver = true;
+}
+
+Grass.prototype = new TileType();
+Grass.prototype.toString = function () {
+  return '.';
+};
+
+//public class Water : TileType
+function Water() {
+  this.name = 'Water';
+  this.canMoveOn = false;
+  this.canShootOver = true;
+}
+
+Water.prototype = new TileType();
+Water.prototype.toString = function () {
+  return '~';
+};
+
+function Rock() {
+  this.name = 'Rock';
+  this.canMoveOn = false;
+  this.canShootOver = true;
+}
+
+Rock.prototype = new TileType();
+Rock.prototype.toString = function () {
+  return '@';
+};
+
+function Tree() {
+  this.name = 'Tree';
+  this.canMoveOn = false;
+  this.canShootOver = false;
+}
+
+Tree.prototype = new TileType();
+Tree.prototype.toString = function () {
+  return 'T';
 };
