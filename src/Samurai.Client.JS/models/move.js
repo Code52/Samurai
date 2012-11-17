02c769b2c274046e05bfa769ac6663b59591f695
Arthur <partylich@gmail.com>
2012-11-17 12:10:50 -0500

    Added Move model

---
+++ src/Samurai.Client.JS/models/move.js
@@ -0,0 +1,26 @@
+function Move(opt) {
+  //public Player Player { get; set; }
+  //public List<Unit> PlayersUnits { get; set; }
+  //public Dictionary<Player, List<Unit>> AffectedUnits { get; set; }
+  this.initialize(opt);
+}
+
+Move.prototype.initialize = function (opt) {
+  var key = null,
+      default_args = {
+        'player' : null,
+        'playersUnits' : [],
+        'affectedUnits' : {}
+      };
+
+  opt || (opt = default_args);
+
+  for(key in default_args) {
+    if(typeof opt[key] == "undefined") opt[key] = default_args[key];
+  }
+
+  // opt[] has all the data - user provided and optional.
+  for(key in opt) {
+    this[key] = opt[key];
+  }
+};
