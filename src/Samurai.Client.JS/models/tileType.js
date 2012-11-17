11e65cc55728fcffce2d770a26c3a48724590809
Arthur <partylich@gmail.com>
2012-11-17 11:44:09 -0500

    Added tileType model

---
+++ src/Samurai.Client.JS/models/tileType.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+function TileType() {
+  this.name = '';
+  this.canMoveOn = false;
+  this.canShootOver = false;
+}
+
+TileType.prototype.toString = function () {
+  //TODO: Implement a string representation of this obj.
+};
