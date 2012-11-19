d549d79d51b419cdf9faf7b68a8ffc62ebcd22de
Arthur <partylich@gmail.com>
2012-11-19 16:10:35 -0500

    Dropped some c# comments

--- src/Samurai.Client.JS/serverApi.js
+++ src/Samurai.Client.JS/serverApi.js
@@ -4,48 +4,45 @@
 function ServerApi(serverUrl) {
   var serverUrl = serverUrl;
 
-//  function createPlayer(string name, Action<CreatePlayerResponse, Exception> callback) {
+
   function createPlayer(name, callback) {
     post("api/players/createPlayer", {name : name}, callback);
   }
 
-//  function login(string name, string key, Action<CreatePlayerResponse, Exception> callback) {
+
   function login(name, key, callback) {
     post("api/players/login", {name : name, token : key}, callback);
   }
 
-//  function getOpenGames(Action<GetOpenGamesResponse, Exception> callback) {
+
   function getOpenGames(callback) {
     get("api/games/getOpenGames", callback);
   }
 
-//  function createGameAndJoin(string name, Guid playerId, Action<CreateGameAndJoinResponse, Exception> callback) {
+
   function createGameAndJoin(name, playerId, callback) {
     post("api/games/createGameAndJoin", {name : name, playerid : playerId}, callback);
   }
 
-//  function joinGame(Guid gameId, Guid playerId, Action<CreateGameAndJoinResponse, Exception> callback) {
+
   function joinGame(gameId, playerId, callback) {
     post("api/games/joinGame", {gameid : gameId, playerid : playerId}, callback);
   }
 
-//  function getMap(Guid mapId, Action<GetMapResponse, Exception> callback) {
+
   function getMap(mapId, callback) {
     post("api/games/getMap", {mapid : mapId}, callback);
   }
 
-//  function startGame(Guid gameId, Action<StartGameResponse, Exception> callback) {
   function startGame(gameId, callback) {
     get("api/games/startGame", callback, {gameid: gameId});
   }
 
-  //function getGame(Guid gameId, Action<GetGameResponse, Exception> callback) {
   function getGame(gameId, callback) {
     get("api/games/getGame", callback, {gameid: gameId});
   }
 
-//A sync method in an async pattern. Easier to work with for the console app.
-//  private void Get<T>(string url, Action<T, Exception> callback) {
+  //Synchronous GET request.
   function get(url, callback, data) {
     try {
       $.ajax(serverUrl + url, {
@@ -62,7 +59,6 @@ function ServerApi(serverUrl) {
         },
       });
     } catch(e) {
-//          callback(default(T), e);
       console.log('Caught an error in serverApi get: ' + e.message);
       callback(null, e);
     }
@@ -77,22 +73,16 @@ function ServerApi(serverUrl) {
         dataType: "json",
         data: data,
         success: function (responseData) {
-//          callback(responseData, null);
             callback(responseData, null);
          },
         error: function (jqXHR, textStatus) {
-//          if (response.StatusCode != HttpStatusCode.OK) callback(default(T), new Exception("Unexpected server response " + response.StatusCode));
+          console.log('Request: ' + url + ' at ' + Date.now());
+          console.log('Data: ', data);
+          console.log('Textstatus', textStatus);
           callback(null, 'Unexpected server response ' + jqXHR.status);
         }
       });
-/******************/
-//          var responseData = JsonConvert.DeserializeObject<T>(json);
-//          console.log('Request: ' + url + ' at ' + Date.Now());
-//          console.log('Data: ', data);
-//          console.log('Response: ');
-//          console.log(json);
     } catch(e) {
-//          callback(default(T), e);
       console.log('Caught an error in serverApi post: ' + e.message);
       callback(null, e);
     }
@@ -121,7 +111,7 @@ function ServerResponse() {
 function GetOpenGamesResponse() {
 //    public GameState[] Games { get; set; }
   ServerResponse.call(this);
-  this.games = [];
+  this.Games = [];
 }
 GetOpenGamesResponse.prototype = new ServerResponse();
 GetOpenGamesResponse.prototype.constructor = GetOpenGamesResponse();
